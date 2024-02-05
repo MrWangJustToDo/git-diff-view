c88f2c03b5f3efca84217bb424178da376ab8389
MrWangJustToDo <2711470541@qq.com>
2024-02-05 17:56:07 +0800

    fix expand button for vue component

--- packages/vue/src/components/DiffUnifiedView.tsx
+++ packages/vue/src/components/DiffUnifiedView.tsx
@@ -1,5 +1,7 @@
 import { getUnifiedContentLine } from "@git-diff-view/core";
-import { Fragment } from "vue";
+import { Fragment, defineComponent, ref } from "vue";
+
+import { useSubscribeDiffFile } from "../hooks/useSubscribeDiffFile";
 
 import { DiffUnifiedExtendLine } from "./DiffUnifiedExtendLine";
 import { DiffUnifiedLastHunkLine, DiffUnifiedHunkLine } from "./DiffUnifiedHunkLine";
@@ -19,39 +21,46 @@ const onMouseDown = (e: MouseEvent) => {
   }
 };
 
-export const DiffUnifiedView = ({ diffFile }: { diffFile: DiffFile }) => {
-  const lines = getUnifiedContentLine(diffFile);
+export const DiffUnifiedView = defineComponent(
+  (props: { diffFile: DiffFile }) => {
+    const lines = ref(getUnifiedContentLine(props.diffFile));
 
-  return (
-    <div class="unified-diff-view w-full">
-      <div
-        class="unified-diff-table-wrapper overflow-auto w-full scrollbar-hide"
-        style={{ fontFamily: "Menlo, Consolas, monospace", fontSize: "var(--diff-font-size--)" }}
-      >
-        <table class="unified-diff-table border-collapse w-full">
-          <colgroup>
-            <col class="unified-diff-table-num-col" />
-            <col class="unified-diff-table-content-col" />
-          </colgroup>
-          <thead class="hidden">
-            <tr>
-              <th scope="col">line number</th>
-              <th scope="col">line content</th>
-            </tr>
-          </thead>
-          <tbody class="diff-table-body leading-[1.4]" onMousedown={onMouseDown}>
-            {lines.map((item) => (
-              <Fragment key={item.index}>
-                <DiffUnifiedHunkLine index={item.index} lineNumber={item.lineNumber} diffFile={diffFile} />
-                <DiffUnifiedLine index={item.index} lineNumber={item.lineNumber} diffFile={diffFile} />
-                <DiffUnifiedWidgetLine index={item.index} lineNumber={item.lineNumber} diffFile={diffFile} />
-                <DiffUnifiedExtendLine index={item.index} lineNumber={item.lineNumber} diffFile={diffFile} />
-              </Fragment>
-            ))}
-            <DiffUnifiedLastHunkLine diffFile={diffFile} />
-          </tbody>
-        </table>
+    useSubscribeDiffFile(props, (diffFile) => {
+      lines.value = getUnifiedContentLine(diffFile);
+    });
+
+    return () => (
+      <div class="unified-diff-view w-full">
+        <div
+          class="unified-diff-table-wrapper overflow-auto w-full scrollbar-hide"
+          style={{ fontFamily: "Menlo, Consolas, monospace", fontSize: "var(--diff-font-size--)" }}
+        >
+          <table class="unified-diff-table border-collapse w-full">
+            <colgroup>
+              <col class="unified-diff-table-num-col" />
+              <col class="unified-diff-table-content-col" />
+            </colgroup>
+            <thead class="hidden">
+              <tr>
+                <th scope="col">line number</th>
+                <th scope="col">line content</th>
+              </tr>
+            </thead>
+            <tbody class="diff-table-body leading-[1.4]" onMousedown={onMouseDown}>
+              {lines.value.map((item) => (
+                <Fragment key={item.index}>
+                  <DiffUnifiedHunkLine index={item.index} lineNumber={item.lineNumber} diffFile={props.diffFile} />
+                  <DiffUnifiedLine index={item.index} lineNumber={item.lineNumber} diffFile={props.diffFile} />
+                  <DiffUnifiedWidgetLine index={item.index} lineNumber={item.lineNumber} diffFile={props.diffFile} />
+                  <DiffUnifiedExtendLine index={item.index} lineNumber={item.lineNumber} diffFile={props.diffFile} />
+                </Fragment>
+              ))}
+              <DiffUnifiedLastHunkLine diffFile={props.diffFile} />
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  },
+  { props: ["diffFile"], name: "DiffUnifiedView" }
+);
