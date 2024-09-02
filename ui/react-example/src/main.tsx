d9625578347120aafa80b07001b1090f668b3c53
MrWangJustToDo <2711470541@qq.com>
2024-09-03 01:21:53 +0800

    update

--- ui/react-example/src/main.tsx
+++ ui/react-example/src/main.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import { __my_react_shared__ } from "@my-react/react";
+
+__my_react_shared__.enableLoopFromRoot.current = true;
 
 import App from "./App.tsx";
 import "@git-diff-view/react/styles/diff-view.css";
