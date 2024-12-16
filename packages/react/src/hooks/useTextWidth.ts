731aec610a9414a3a28d84c12d448c0125fd8142
MrWangJustToDo <2711470541@qq.com>
2024-12-16 18:45:17 +0800

    improve

--- packages/react/src/hooks/useTextWidth.ts
+++ packages/react/src/hooks/useTextWidth.ts
@@ -1,49 +1,7 @@
+import { getTextMeasureInstance } from "@git-diff-view/utils";
 import { useState } from "react";
 
 import { useSafeLayout } from "./useSafeLayout";
-
-let canvasCtx: CanvasRenderingContext2D | null = null;
-
-const getKey = (font: { fontFamily?: string; fontStyle?: string; fontSize?: string }, text: string) => {
-  return `${font.fontFamily}-${font.fontStyle}-${font.fontSize}-${text}`;
-};
-
-const getStableKey = (font: { fontFamily?: string; fontStyle?: string; fontSize?: string }, text: string) => {
-  return getKey(font, "0".repeat(text.length));
-};
-
-class TextMeasure {
-  #key: string = "";
-
-  #map: Record<string, number> = {};
-
-  #getInstance(): CanvasRenderingContext2D {
-    canvasCtx = canvasCtx || document.createElement("canvas").getContext("2d");
-    return canvasCtx!;
-  }
-
-  measure(text: string, font?: { fontFamily?: string; fontStyle?: string; fontSize?: string }) {
-    const currentKey = getStableKey(font, text);
-    if (this.#map[currentKey]) {
-      return this.#map[currentKey];
-    }
-    const instance = this.#getInstance();
-    if (font) {
-      const currentFontKey = `${font.fontFamily}-${font.fontStyle}-${font.fontSize}`;
-      if (this.#key !== currentFontKey) {
-        this.#key = currentFontKey;
-        instance.font = `${font.fontStyle || ""} ${font.fontSize || ""} ${font.fontFamily || ""}`;
-      }
-    } else {
-      instance.font = "";
-    }
-    const textWidth = instance.measureText(text).width;
-
-    return textWidth;
-  }
-}
-
-const measureInstance = new TextMeasure();
 
 export const useTextWidth = ({
   text,
@@ -60,7 +18,7 @@ export const useTextWidth = ({
   });
 
   useSafeLayout(() => {
-    const width = measureInstance.measure(text, font);
+    const width = getTextMeasureInstance().measure(text, font);
 
     setWidth(width);
   }, [text, font]);
