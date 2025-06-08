f36655b4e4fc0ed4b1afe45a8609e1583e423bbe
MrWangJustToDo <2711470541@qq.com>
2025-06-08 22:13:25 +0800

    update

--- ui/react-example/src/components/PlayGroundContent.tsx
+++ ui/react-example/src/components/PlayGroundContent.tsx
@@ -1,4 +1,4 @@
-import { escapeHtml, generateDiffFile, setTransformForContent } from "@git-diff-view/file";
+import { /* escapeHtml, */ generateDiffFile, /* setTransformForContent */ } from "@git-diff-view/file";
 import { DiffView, DiffFile } from "@git-diff-view/react";
 import { Button, Code, Switch, useMantineColorScheme } from "@mantine/core";
 import { debounce } from "lodash";
@@ -8,11 +8,11 @@ import { useTabWithRouter } from "../hooks/useTabWithRouter";
 
 // 支持区分 tab 空格
 // 其中 tab 用符号 → 空格用符号 ·
-setTransformForContent((content: string) => {
-  return escapeHtml(content)
-    .replace(/ /g, '<span style="color: gray;">·</span>')
-    .replace(/\t/g, '<span style="color: gray;">→</span>');
-});
+// setTransformForContent((content: string) => {
+//   return escapeHtml(content)
+//     .replace(/ /g, '<span style="color: gray;">·</span>')
+//     .replace(/\t/g, '<span style="color: gray;">→</span>');
+// });
 
 const temp =
   'diff --git a/packages/myreact-reactivity/src/reactive/feature.ts b/packages/myreact-reactivity/src/reactive/feature.ts\nindex 5b301628..15aac42f 100644\n--- a/packages/myreact-reactivity/src/reactive/feature.ts\n+++ b/packages/myreact-reactivity/src/reactive/feature.ts\n@@ -74,7 +74,7 @@ export function createReactive<P extends Record<string, unknown>, S extends Reco\n \n     componentWillUnmount(): void {\n       this.props.$$__instance__$$.onUnmounted.forEach((f) => f());\n-      this.effect.stop();\n+      this.reactiveEffect.stop();\n     }\n \n     shouldComponentUpdate(): boolean {\n@@ -84,7 +84,7 @@ export function createReactive<P extends Record<string, unknown>, S extends Reco\n       return true;\n     }\n \n-    effect = new ReactiveEffect(() => {\n+    reactiveEffect = new ReactiveEffect(() => {\n       const { children, $$__trigger__$$, $$__reactiveState__$$, $$__instance__$$, ...last } = this.props;\n       const targetRender = (render || children) as (props: UnwrapRef<S> & P) => LikeReactNode;\n       const element = targetRender?.({ ...last, ...$$__reactiveState__$$ } as UnwrapRef<S> & P) || null;\n@@ -92,7 +92,7 @@ export function createReactive<P extends Record<string, unknown>, S extends Reco\n     }, this.props.$$__trigger__$$);\n \n     render() {\n-      return createElement(ForBeforeMount, { ["$$__instance__$$"]: this.props.$$__instance__$$, children: this.effect.run() });\n+      return createElement(ForBeforeMount, { ["$$__instance__$$"]: this.props.$$__instance__$$, children: this.reactiveEffect.run() });\n     }\n   }\n \n@@ -104,10 +104,10 @@ export function createReactive<P extends Record<string, unknown>, S extends Reco\n     } & P\n   > {\n     componentWillUnmount(): void {\n-      this.effect.stop();\n+      this.reactiveEffect.stop();\n     }\n \n-    effect = new ReactiveEffect(() => {\n+    reactiveEffect = new ReactiveEffect(() => {\n       const { children, $$__trigger__$$, $$__reactiveState__$$, $$__instance__$$, ...last } = this.props;\n       const targetRender = (render || children) as (props: UnwrapRef<S> & P) => LikeReactNode;\n       const element = targetRender?.({ ...last, ...$$__reactiveState__$$ } as UnwrapRef<S> & P) || null;\n@@ -115,7 +115,7 @@ export function createReactive<P extends Record<string, unknown>, S extends Reco\n     }, this.props.$$__trigger__$$);\n \n     render() {\n-      return this.effect.run();\n+      return this.reactiveEffect.run();\n     }\n   }\n';
