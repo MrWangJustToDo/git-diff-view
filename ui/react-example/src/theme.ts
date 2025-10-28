28b74882c7672adee465ef73ae925f68f9891706
MrWangJustToDo <2711470541@qq.com>
2025-10-28 13:37:29 +0800

    update

--- ui/react-example/src/theme.ts
+++ ui/react-example/src/theme.ts
@@ -1,5 +1,5 @@
 import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
-  fontFamily: "Josefin Sans, consolas, monospace",
+  fontFamily: "Open Sans Variable, consolas, monospace",
 });
